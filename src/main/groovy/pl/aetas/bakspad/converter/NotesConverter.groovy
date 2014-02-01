7dd9dc6d52a8f6122fa05f1131b14fc4dbc78f49
mariusz <mariusz@aetas.pl>
2014-02-01 15:50:46 +0000

    Converter prepared to convert n to br and to make a backup first

--- src/main/groovy/pl/aetas/bakspad/converter/NotesConverter.groovy
+++ src/main/groovy/pl/aetas/bakspad/converter/NotesConverter.groovy
@@ -1,9 +1,13 @@
 package pl.aetas.bakspad.converter
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
 
 import java.nio.file.FileSystems
 import java.nio.file.Path
 
 class NotesConverter {
+
+    private final static Logger LOGGER = LogManager.getLogger();
 
     private final Path dataDirectory
 
@@ -21,14 +25,30 @@ class NotesConverter {
 //    }
 
     private static def processFileInPlace(File file, Closure processText) {
+        LOGGER.info("Processing file {}", file.name)
         def text = file.text
         file.write(processText(text))
     }
 
-    static main(String[] args) {
+    static void main(String[] args) {
         Path dataDirectory = FileSystems.getDefault().getPath("data");
-        def converter = new NotesConverter(dataDirectory);
+        def converter = new NotesConverter(dataDirectory)
+        converter.backupDataFiles(FileSystems.getDefault().getPath("data-backup"))
+        LOGGER.info("Converting \\\\n to <br /> started")
         converter.processNotesAsString(replaceNewLineWithHtmlBrTag)
+        LOGGER.info("Converting \\\\n to <br /> finished")
+
+    }
+
+    def backupDataFiles(Path destinationPath) {
+        LOGGER.info("Creating backup from {} to {}",
+                dataDirectory.toAbsolutePath().toString(), destinationPath.toAbsolutePath().toString())
+
+        new AntBuilder().copy( todir: destinationPath.toAbsolutePath().toString()) {
+            fileset( dir: dataDirectory.toAbsolutePath().toString())
+        }
+
+        LOGGER.info("Backup completed")
     }
 
     static def Closure replaceNewLineWithHtmlBrTag = {
